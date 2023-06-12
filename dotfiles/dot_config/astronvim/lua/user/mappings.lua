3175a1ed75e089b071df8860e0397d688f61235b
Pablo Jaramillo <juan@pablox.io>
2023-06-12 14:08:18 -0600

    [nvim] Telescope current buffer fuzzy find

--- dotfiles/dot_config/astronvim/lua/user/mappings.lua
+++ dotfiles/dot_config/astronvim/lua/user/mappings.lua
@@ -5,5 +5,7 @@ return {
     ["<leader>tT"] = { "<cmd>tabnew term://zsh<cr>", desc = "New terminal in a new tab" },
     ["<leader>tx"] = { "<cmd>split term://zsh<cr>", desc = "New terminal split horizontal" },
     ["<leader>tv"] = { "<cmd>vsplit term://zsh<cr>", desc = "New terminal split vertical" },
+    -- Telescope
+    ["<leader>f/"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find current buffer" },
   },
 }
