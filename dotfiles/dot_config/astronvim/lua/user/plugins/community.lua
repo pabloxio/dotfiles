adfc72b9f6ec3dcb7138cc4cbce8c444ffb257fa
Pablo Jaramillo <juan@pablox.io>
2023-06-21 14:13:19 -0600

    [nvim] add community packs for multiple lang/frameworks

--- dotfiles/dot_config/astronvim/lua/user/plugins/community.lua
+++ dotfiles/dot_config/astronvim/lua/user/plugins/community.lua
@@ -6,4 +6,21 @@ return {
 	{ import = "astrocommunity.bars-and-lines.bufferline-nvim" },
 	-- Completion
 	{ import = "astrocommunity.completion.copilot-lua-cmp" },
+	-- Languages/Framework/etc Packs
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.docker" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.nix" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.terraform" },
+	{ import = "astrocommunity.pack.toml" },
+	{ import = "astrocommunity.pack.typescript-all-in-one" },
+	{ import = "astrocommunity.pack.vue" },
+	{ import = "astrocommunity.pack.yaml" },
 }
