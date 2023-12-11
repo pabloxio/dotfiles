fa45cfeed61743df43829bf456eb82258cef6348
Pablo Jaramillo <juan@pablox.io>
2023-12-10 20:00:23 -0600

    [zsh] aliases for aws-vault, kubectl, and k9s

--- dotfiles/dot_zsh.d/aliases.zsh
+++ dotfiles/dot_zsh.d/aliases.zsh
@@ -4,6 +4,23 @@ if which bat >/dev/null; then
   alias cat='bat'
 fi
 
+# aws-vault
+if which aws-vault >/dev/null; then
+  alias av-exec='aws-vault exec $AWSVAULT --duration=${AWSVAULT_DURATION:-1h} -- '
+  alias av-creds='aws-vault exec $AWSVAULT --duration=1h -- env |grep ^AWS_ |while read line; do echo export "$line"; done'
+  alias av-login='aws-vault login $AWSVAULT'
+fi
+
 if which kubectl >/dev/null; then
-  alias kube='kubectl'
+  if which aws-vault >/dev/null; then
+    alias kube='av-exec kubectl'
+  else
+    alias kube='kubectl'
+  fi
+fi
+
+if which k9s >/dev/null; then
+  if which aws-vault >/dev/null; then
+    alias k9s='av-exec k9s'
+  fi
 fi
