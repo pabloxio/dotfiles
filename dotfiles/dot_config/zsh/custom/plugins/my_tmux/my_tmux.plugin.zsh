3e132757d957ec8f68f75933f38469f10774369e
Pablo Jaramillo <juan@pablox.io>
2024-01-22 10:44:37 -0600

    [zsh] custom my_tmux plugin

---
+++ dotfiles/dot_config/zsh/custom/plugins/my_tmux/my_tmux.plugin.zsh
@@ -0,0 +1,18 @@
+# my_tmux
+#
+# Auto open tmux sessions when an empty .tmux file
+# is present. The Tmux session name will be the directory name.
+#
+# https://zsh.sourceforge.io/Doc/Release/Functions.html#Hook-Functions
+
+command -v tmux &>/dev/null || return
+
+_my_tmux_hook() {
+  if [ -f .tmux ] && [ -z $TMUX ]; then
+    tmux new-session -s $(basename $(pwd))
+  fi
+}
+
+# Loading hook function into chpwd special function
+autoload -U add-zsh-hook
+add-zsh-hook chpwd _my_tmux_hook
